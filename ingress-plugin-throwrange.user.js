c8b4ba4ba3e30940cd16856cd1b99638d8ce63a7
Brandon Smith <blsmit5728@gmail.com>
2022-05-27 07:27:44 -0400

    Test

--- ingress-plugin-throwrange.user.js
+++ ingress-plugin-throwrange.user.js
@@ -3,7 +3,7 @@
 // @name           Under-Field Throw Range
 // @author         abeerslayer
 // @category       Layer
-// @version        1.0.0
+// @version        1.1.0
 // @downloadURL    https://github.com/blsmit5728/iitc_plugins/raw/main/ingress-plugin-throwrange.user.js
 // @updateURL      https://github.com/blsmit5728/iitc_plugins/raw/main/ingress-plugin-throwrange.user.js
 // @description    Shows under field throw range at 500m
@@ -14,274 +14,286 @@
 
 
 
-  
- /* globals dialog */
-  
- function wrapper(plugin_info) {
-  // Make sure that window.plugin exists. IITC defines it as a no-op function,
-  // and other plugins assume the same.
-  if (typeof window.plugin !== "function") window.plugin = function () {};
 
-  const KEY_SETTINGS = "FieldThrowrange-settings";
+/* globals dialog */
 
-  // Use own namespace for plugin
-  window.plugin.FieldThrowrange = function () {};
+function wrapper(plugin_info) {
+    // Make sure that window.plugin exists. IITC defines it as a no-op function,
+    // and other plugins assume the same.
+    if (typeof window.plugin !== "function") window.plugin = function () { };
 
-  const thisPlugin = window.plugin.FieldThrowrange;
+    const KEY_SETTINGS = "FieldThrowrange-settings";
 
-  // Name of the IITC build for first-party plugins
-  plugin_info.buildName = "FieldThrowrange";
+    // Use own namespace for plugin
+    window.plugin.FieldThrowrange = function () { };
 
-  // Datetime-derived version of the plugin
-  plugin_info.dateTimeVersion = "20201112000000";
+    const thisPlugin = window.plugin.FieldThrowrange;
 
-  // ID/name of the plugin
-  plugin_info.pluginId = "FieldThrowrange";
+    // Name of the IITC build for first-party plugins
+    plugin_info.buildName = "FieldThrowrange";
 
-  const TIMERS = {};
-  function createThrottledTimer(name, callback, ms) {
-      if (TIMERS[name]) clearTimeout(TIMERS[name]);
+    // Datetime-derived version of the plugin
+    plugin_info.dateTimeVersion = "20201112000000";
 
-      // throttle if there are several calls to the functions
-      TIMERS[name] = setTimeout(function () {
-          delete TIMERS[name];
-          if (typeof window.requestIdleCallback == "undefined") callback();
-          // and even now, wait for iddle
-          else
-              requestIdleCallback(
-                  function () {
-                      callback();
-                  },
-                  { timeout: 2000 }
-              );
-      }, ms || 100);
-  }
+    // ID/name of the plugin
+    plugin_info.pluginId = "FieldThrowrange";
 
-  window.portalFieldThrowrangeIndicator  = null;
-  FieldThrowrangeLayer = null;
-  dFieldThrowrangeLayerGroup = null;
-  let lastPortalGuid = null;
+    const TIMERS = {};
+    function createThrottledTimer(name, callback, ms) {
+        if (TIMERS[name]) clearTimeout(TIMERS[name]);
 
-  map = window.map;
+        // throttle if there are several calls to the functions
+        TIMERS[name] = setTimeout(function () {
+            delete TIMERS[name];
+            if (typeof window.requestIdleCallback == "undefined") callback();
+            // and even now, wait for iddle
+            else
+                requestIdleCallback(
+                    function () {
+                        callback();
+                    },
+                    { timeout: 2000 }
+                );
+        }, ms || 100);
+    }
 
-  const defaultSettings = {
-      circleColor: "#800080",
-      circleWidth: 2,
-      showAllResoCircle: false
-  };
+    window.portalFieldThrowrangeIndicator = null;
+    FieldThrowrangeLayer = null;
+    dFieldThrowrangeLayerGroup = null;
+    let lastPortalGuid = null;
 
-  let settings = defaultSettings;
+    map = window.map;
 
-  function saveSettings() {
-      createThrottledTimer("saveSettings", function () {
-          localStorage[KEY_SETTINGS] = JSON.stringify(settings);
-      });
-      drawFieldThrowRange(lastPortalGuid);
-  }
+    const defaultSettings = {
+        circleColor: "#800080",
+        circleWidth: 2,
+        circleRange: 500,
+        showAllResoCircle: false
+    };
 
-  thisPlugin.loadSettings = function() {
-      const tmp = localStorage[KEY_SETTINGS];
-      try {
-          settings = JSON.parse(tmp);
-      } catch (e) {
-          // eslint-disable-line no-empty
-      }
-      if (!settings.circleWidth) {
-          settings.circleWidth = "2";
-      }
-      if (!"showAllResoCircle" in settings) {
-          settings.showAllResoCircle = false
-      }
-  }
+    let settings = defaultSettings;
 
-  window.resetSettings = function() {
-      settings = JSON.parse(JSON.stringify(defaultSettings));
-      showSettingsDialog();
-  }
+    function saveSettings() {
+        createThrottledTimer("saveSettings", function () {
+            localStorage[KEY_SETTINGS] = JSON.stringify(settings);
+        });
+        drawFieldThrowRange(lastPortalGuid);
+    }
 
-  // The entry point for this plugin.
-  function setup() {
-      thisPlugin.loadSettings();
+    thisPlugin.loadSettings = function () {
+        const tmp = localStorage[KEY_SETTINGS];
+        try {
+            settings = JSON.parse(tmp);
+        } catch (e) {
+            // eslint-disable-line no-empty
+        }
+        if (!settings.circleWidth) {
+            settings.circleWidth = "2";
+        }
+        if (!settings.circleRange) {
+            settings.circleRange = 500;
+        }
+        if (!"showAllResoCircle" in settings) {
+            settings.showAllResoCircle = false
+        }
+    }
 
-      window.addHook(
-          "portalSelected",
-          window.drawFieldThrowRange
-      );
+    window.resetSettings = function () {
+        settings = JSON.parse(JSON.stringify(defaultSettings));
+        showSettingsDialog();
+    }
 
-      FieldThrowrangeLayer = L.layerGroup();
-      window.addLayerGroup('Under Field Throw Range', FieldThrowrangeLayer, true);
-      dFieldThrowrangeLayerGroup = L.layerGroup();
+    // The entry point for this plugin.
+    function setup() {
+        thisPlugin.loadSettings();
 
-      const toolbox = document.getElementById("toolbox");
+        window.addHook(
+            "portalSelected",
+            window.drawFieldThrowRange
+        );
 
-      let buttonFieldThrowrange = document.createElement("a");
-      buttonFieldThrowrange.textContent = "Under Field Throw Range Settings";
-      buttonFieldThrowrange.title = "Configuration for Under Field Throw Range Plugin";
-      buttonFieldThrowrange.addEventListener("click", showSettingsDialog);
-      toolbox.appendChild(buttonFieldThrowrange);
-  }
+        FieldThrowrangeLayer = L.layerGroup();
+        window.addLayerGroup('Under Field Throw Range', FieldThrowrangeLayer, true);
+        dFieldThrowrangeLayerGroup = L.layerGroup();
 
-  thisPlugin.saveToFile = function(text, filename) {
-      if (typeof text != 'string') {
-          text = JSON.stringify(text);
-      }
+        const toolbox = document.getElementById("toolbox");
 
-      if (typeof window.saveFile != 'undefined') {
-          window.saveFile(text, filename, 'application/json');
-          return;
-      }
-  };
+        let buttonFieldThrowrange = document.createElement("a");
+        buttonFieldThrowrange.textContent = "Under Field Throw Range Settings";
+        buttonFieldThrowrange.title = "Configuration for Under Field Throw Range Plugin";
+        buttonFieldThrowrange.addEventListener("click", showSettingsDialog);
+        toolbox.appendChild(buttonFieldThrowrange);
+    }
 
-  thisPlugin.readFromFile = function(callback) {
-      if (typeof L.FileListLoader != 'undefined') {
-          L.FileListLoader.loadFiles({accept: 'application/json'})
-              .on('load',function (e) {
-                  callback(e.reader.result);
-              });
-          return;
-      }
-  };
+    thisPlugin.saveToFile = function (text, filename) {
+        if (typeof text != 'string') {
+            text = JSON.stringify(text);
+        }
 
-  function showSettingsDialog() {
-      const html =
-                  `<p><label for="colorCircleColor">Radius Circle Color</label><br><input type="color" id="colorCircleColor" /></p>
+        if (typeof window.saveFile != 'undefined') {
+            window.saveFile(text, filename, 'application/json');
+            return;
+        }
+    };
+
+    thisPlugin.readFromFile = function (callback) {
+        if (typeof L.FileListLoader != 'undefined') {
+            L.FileListLoader.loadFiles({ accept: 'application/json' })
+                .on('load', function (e) {
+                    callback(e.reader.result);
+                });
+            return;
+        }
+    };
+
+    function showSettingsDialog() {
+        const html =
+            `<p><label for="colorCircleColor">Radius Circle Color</label><br><input type="color" id="colorCircleColor" /></p>
                    <p><label for="textCircleWidth">Radius Circle Thickness</label><br><input type="text" id="textCircleWidth" /></p>
+                   <p><label for="textCircleRange">Throw Range KM</label><br><input type="text" id="textCircleRange" /></p>
                    <a onclick="window.resetSettings();return false;" title="Restores settings to default state">Reset to Defaults</a>
                   `;
 
-      const width = Math.min(screen.availWidth, 420);
-      const container = dialog({
-          id: "settings",
-          width: width + "px",
-          html: html,
-          title: "Under Field Throw Range Settings",
-      });
+        const width = Math.min(screen.availWidth, 420);
+        const container = dialog({
+            id: "settings",
+            width: width + "px",
+            html: html,
+            title: "Under Field Throw Range Settings",
+        });
 
-      const div = container[0];
+        const div = container[0];
 
-      const colorCircleColorPicker = div.querySelector("#colorCircleColor");
-      colorCircleColorPicker.value = settings.circleColor;
-      colorCircleColorPicker.addEventListener("change", (e) => {
-          settings.circleColor = colorCircleColorPicker.value;
-          saveSettings();
-      });
+        const colorCircleColorPicker = div.querySelector("#colorCircleColor");
+        colorCircleColorPicker.value = settings.circleColor;
+        colorCircleColorPicker.addEventListener("change", (e) => {
+            settings.circleColor = colorCircleColorPicker.value;
+            saveSettings();
+        });
 
-      const textCircleWidthStr = div.querySelector("#textCircleWidth");
-      textCircleWidthStr.value = settings.circleWidth;
-      textCircleWidthStr.addEventListener("change", (e) => {
-          settings.circleWidth = textCircleWidthStr.value;
-          saveSettings();
-      });
+        const textCircleWidthStr = div.querySelector("#textCircleWidth");
+        textCircleWidthStr.value = settings.circleWidth;
+        textCircleWidthStr.addEventListener("change", (e) => {
+            settings.circleWidth = textCircleWidthStr.value;
+            saveSettings();
+        });
 
-      const showAllCB = div.querySelector("#cbShowAllResoCircle");
-      showAllCB.checked = settings.showAllResoCircle;
-      showAllCB.addEventListener("change", (e) => {
-          settings.showAllResoCircle = showAllCB.checked;
-          saveSettings();
-      });
-  };
+        const textCircleRange = div.querySelector("#textCircleRange");
+        textCircleRange.value = settings.circleRange;
+        textCircleRange.addEventListener("change", (e) => {
+            settings.circleRange = textCircleRange.value;
+            saveSettings();
+        });
 
-  window.drawFieldThrowRange = function (guid) {
-      portalFieldThrowrangeIndicator = null;
-      dFieldThrowrangeLayerGroup.clearLayers();
+        const showAllCB = div.querySelector("#cbShowAllResoCircle");
+        showAllCB.checked = settings.showAllResoCircle;
+        showAllCB.addEventListener("change", (e) => {
+            settings.showAllResoCircle = showAllCB.checked;
+            saveSettings();
+        });
+    };
 
-      if (guid) {
-          if (guid.selectedPortalGuid) {
-              lastPortalGuid = guid;
+    window.drawFieldThrowRange = function (guid) {
+        portalFieldThrowrangeIndicator = null;
+        dFieldThrowrangeLayerGroup.clearLayers();
 
-              p = window.portals[guid.selectedPortalGuid];
-              if (p) {
-                  const coord = new LatLng(p._latlng.lat, p._latlng.lng);
-                  portalFieldThrowrangeIndicator = L.circle(coord, 500,
-                      { fill: false, color: settings.circleColor, weight: settings.circleWidth, interactive: false }
-                  )
-                  dFieldThrowrangeLayerGroup.addLayer(portalFieldThrowrangeIndicator);
-              }
-              updateMap();
-          } else {
-              if (FieldThrowrangeLayer.hasLayer(dFieldThrowrangeLayerGroup)) {
-                  FieldThrowrangeLayer.removeLayer(dFieldThrowrangeLayerGroup);
-              }
-          }
-      }
-  };
+        if (guid) {
+            if (guid.selectedPortalGuid) {
+                lastPortalGuid = guid;
 
-  setup.info = plugin_info; //add the script info data to the function as a property
-  // if IITC has already booted, immediately run the 'setup' function
-  if (window.iitcLoaded) {
-      setup();
-      } else {
-          if (!window.bootPlugins) {
-              window.bootPlugins = [];
-          }
-      window.bootPlugins.push(setup);
-  }
+                p = window.portals[guid.selectedPortalGuid];
+                if (p) {
+                    const coord = new LatLng(p._latlng.lat, p._latlng.lng);
+                    portalFieldThrowrangeIndicator = L.circle(coord, settings.circleRange,
+                        { fill: false, color: settings.circleColor, weight: settings.circleWidth, interactive: false }
+                    )
+                    dFieldThrowrangeLayerGroup.addLayer(portalFieldThrowrangeIndicator);
+                }
+                updateMap();
+            } else {
+                if (FieldThrowrangeLayer.hasLayer(dFieldThrowrangeLayerGroup)) {
+                    FieldThrowrangeLayer.removeLayer(dFieldThrowrangeLayerGroup);
+                }
+            }
+        }
+    };
 
-  function updateMap() {
-      if (!portalFieldThrowrangeIndicator) {
-          return;
-      }
+    setup.info = plugin_info; //add the script info data to the function as a property
+    // if IITC has already booted, immediately run the 'setup' function
+    if (window.iitcLoaded) {
+        setup();
+    } else {
+        if (!window.bootPlugins) {
+            window.bootPlugins = [];
+        }
+        window.bootPlugins.push(setup);
+    }
 
-      const zoom = map.getZoom();
+    function updateMap() {
+        if (!portalFieldThrowrangeIndicator) {
+            return;
+        }
 
-      if (zoom > 8) {
-          if (!FieldThrowrangeLayer.hasLayer(dFieldThrowrangeLayerGroup)) {
-              FieldThrowrangeLayer.addLayer(dFieldThrowrangeLayerGroup);
-          }
-      }
-  }
+        const zoom = map.getZoom();
 
-  function LatLng(lat, lng, alt) {
-      if (isNaN(lat) || isNaN(lng)) {
-          throw new Error('Invalid LatLng object: (' + lat + ', ' + lng + ')');
-      }
+        if (zoom > 8) {
+            if (!FieldThrowrangeLayer.hasLayer(dFieldThrowrangeLayerGroup)) {
+                FieldThrowrangeLayer.addLayer(dFieldThrowrangeLayerGroup);
+            }
+        }
+    }
 
-      // @property lat: Number
-      // Latitude in degrees
-      this.lat = +lat;
+    function LatLng(lat, lng, alt) {
+        if (isNaN(lat) || isNaN(lng)) {
+            throw new Error('Invalid LatLng object: (' + lat + ', ' + lng + ')');
+        }
 
-      // @property lng: Number
-      // Longitude in degrees
-      this.lng = +lng;
+        // @property lat: Number
+        // Latitude in degrees
+        this.lat = +lat;
 
-      // @property alt: Number
-      // Altitude in meters (optional)
-      if (alt !== undefined) {
-          this.alt = +alt;
-      }
-  }
+        // @property lng: Number
+        // Longitude in degrees
+        this.lng = +lng;
 
-  LatLng.prototype = {
+        // @property alt: Number
+        // Altitude in meters (optional)
+        if (alt !== undefined) {
+            this.alt = +alt;
+        }
+    }
 
-      // @method toString(): String
-      // Returns a string representation of the point (for debugging purposes).
-      toString: function (precision) {
-          return 'LatLng(' +
-                  formatNum(this.lat, precision) + ', ' +
-                  formatNum(this.lng, precision) + ')';
-      }
+    LatLng.prototype = {
 
-  };
+        // @method toString(): String
+        // Returns a string representation of the point (for debugging purposes).
+        toString: function (precision) {
+            return 'LatLng(' +
+                formatNum(this.lat, precision) + ', ' +
+                formatNum(this.lng, precision) + ')';
+        }
+
+    };
 
 }
 
 (function () {
-  const plugin_info = {};
-  if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) {
-      plugin_info.script = {
-          version: GM_info.script.version,
-          name: GM_info.script.name,
-          description: GM_info.script.description
-      };
-  }
-  // Greasemonkey. It will be quite hard to debug
-  if (typeof unsafeWindow != 'undefined' || typeof GM_info == 'undefined' || GM_info.scriptHandler != 'Tampermonkey') {
-  // inject code into site context
-      const script = document.createElement('script');
-      script.appendChild(document.createTextNode('(' + wrapper + ')(' + JSON.stringify(plugin_info) + ');'));
-      (document.body || document.head || document.documentElement).appendChild(script);
-  } else {
-      // Tampermonkey, run code directly
-      wrapper(plugin_info);
-  }
+    const plugin_info = {};
+    if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) {
+        plugin_info.script = {
+            version: GM_info.script.version,
+            name: GM_info.script.name,
+            description: GM_info.script.description
+        };
+    }
+    // Greasemonkey. It will be quite hard to debug
+    if (typeof unsafeWindow != 'undefined' || typeof GM_info == 'undefined' || GM_info.scriptHandler != 'Tampermonkey') {
+        // inject code into site context
+        const script = document.createElement('script');
+        script.appendChild(document.createTextNode('(' + wrapper + ')(' + JSON.stringify(plugin_info) + ');'));
+        (document.body || document.head || document.documentElement).appendChild(script);
+    } else {
+        // Tampermonkey, run code directly
+        wrapper(plugin_info);
+    }
 })();
