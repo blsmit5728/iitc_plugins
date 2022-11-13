b748698396758f9d3b285a51e73036ddb0df72ae
Brandon Smith <blsmit5728@gmail.com>
2022-11-13 11:01:53 -0500

    Update ingress-plugin-highlight-red.user.js

--- ingress-plugin-highlight-red.user.js
+++ ingress-plugin-highlight-red.user.js
@@ -1,71 +1,60 @@
 // ==UserScript==
-// @author         ABeerSlayer
-// @name           IITC plugin: Highlight Red portals
-// @description    Highlight red portals
+// @id             iitc-plugin-highlight-red-portals@abeerslayer
+// @name           IITC plugin: highlight Red Portals
 // @category       Highlighter
-// @version        0.0.1
-// @id             highlight-red-portals
+// @version        0.0.0.1
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://static.iitc.me/build/release/plugins/portal-highlighter-portals-my-level.meta.js
+// @downloadURL    https://static.iitc.me/build/release/plugins/portal-highlighter-portals-my-level.user.js
+// @description    [iitc-2017-01-08-021732] Use the portal fill color to denote if the portal is either at and above, or at and below your level.
 // @match          https://*.ingress.com/*
-// @match          http://*.ingress.com/*
-// @match          https://*.ingress.com/mission/*
-// @match          http://*.ingress.com/mission/*
-// @include        https://*.ingress.com/*
-// @include        http://*.ingress.com/*
-// @include        https://*.ingress.com/mission/*
-// @include        http://*.ingress.com/mission/*
 // @grant          none
-// @namespace 
 // ==/UserScript==
 
+
 function wrapper(plugin_info) {
-    if(typeof window.plugin !== 'function') window.plugin = function() {};
+// ensure plugin framework is there, even if iitc is not yet loaded
+if(typeof window.plugin !== 'function') window.plugin = function() {};
 
-    plugin_info.buildName = 'ABeerSlayer@highlight-red-portals';
-    plugin_info.dateTimeVersion = '2021-02-08-220302';
-    plugin_info.pluginId = 'highlight-red-portals';
+//PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
+//(leaving them in place might break the 'About IITC' page or break update checks)
+plugin_info.buildName = 'iitc';
+plugin_info.dateTimeVersion = '20221113.000000';
+plugin_info.pluginId = 'portal-highlighter-portals-my-level';
+//END PLUGIN AUTHORS NOTE
 
-    function highlight(data, flag) {    
-            data.portal.setStyle({fillColor: '#ff0000', fillOpacity: 0.75});
+
+
+// PLUGIN START ////////////////////////////////////////////////////////
+
+// use own namespace for plugin
+window.plugin.redPortalHighLight = function() {};
+
+
+window.plugin.redPortalHighLight.red = function(data) {
+
+    if( data.portal.options.data.team == 'M' ){
+        data.portal.setStyle({fillColor: '#FF0000', fillOpacity: 1.0});
     }
 
-    var e = {};
-
-    e.highlightRedPortals = function(data) {
-        //highlight(data, CAPTURED);
-    }
-
-    e.handlePortalSelect = function(data) {
-        setTimeout(function() {
-            highlightPortal(data.portal);
-        },0);
-    }
-
-    window.plugin.highlightPortals = e;
-
-    function setup() {
-        window.addPortalHighlighter('Red', window.plugin.highlightPortals.highlightRedPortals);
-
-        window.addHook('portalDetailsUpdated', window.plugin.highlightPortals.handlePortalSelect);
-    }
-
-    setup.info = plugin_info;
-
-    if (!window.bootPlugins) window.bootPlugins = [];
-    window.bootPlugins.push(setup);
-    if (window.iitcLoaded && typeof setup === 'function') setup();
 }
 
+var setup = function() {
+  window.addPortalHighlighter('Red Portals', window.plugin.redPortalHighLight.red);
+}
+
+// PLUGIN END //////////////////////////////////////////////////////////
+
+
+setup.info = plugin_info; //add the script info data to the function as a property
+if(!window.bootPlugins) window.bootPlugins = [];
+window.bootPlugins.push(setup);
+// if IITC has already booted, immediately run the 'setup' function
+if(window.iitcLoaded && typeof setup === 'function') setup();
+} // wrapper end
+// inject code into site context
 var script = document.createElement('script');
 var info = {};
-
-if(typeof GM_info !== 'undefined' && GM_info && GM_info.script) {
-    info.script = {
-        version: GM_info.script.version,
-        name: GM_info.script.name,
-        description: GM_info.script.description
-    };
-}
-
-var textContent = document.createTextNode('('+ wrapper +')('+ JSON.stringify(info) +')');
-script.appendChild(textContent);
+if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = { version: GM_info.script.version, name: GM_info.script.name, description: GM_info.script.description };
+script.appendChild(document.createTextNode('('+ wrapper +')('+JSON.stringify(info)+');'));
 (document.body || document.head || document.documentElement).appendChild(script);
