d599fda79fc7edde58d252ca09c6259eaf80d7ef
Brandon Smith <blsmit5728@gmail.com>
2023-08-24 13:48:54 -0400

    Created a copy for inventory

---
+++ ingress-plugin-range-calc.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         IITC Range Calculator
+// @namespace    http://github.com
+// @version      0.1
+// @description  Calculate the Throw Range from a portal
+// @author       ABeerSlayer
+// @match        *://intel.ingress.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
